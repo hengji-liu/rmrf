96c2c4d0e59d8b83e389799838657cc0a648c868
ge90toga <liquan1992@outlook.com>
2016-09-03 16:20:00 +1000

    Starting Point 03/09/2016

---
+++ src/util/EmailUtil.java
@@ -0,0 +1,59 @@
+package util;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailUtil {
+
+    private static final String  SENDER_ADDR= "rmrf.9321.service@gmail.com";
+    private static final String PASSWORD = "9321Sucks";
+
+    /**
+     * Sending simple email.
+     * @param subject the subject of email e.g. Registration Confirmation
+     * @param content content of email e.g. we have received your registration, please confirm via...
+     * @param recipientAddress recipientAddress e.g. john@gmail.com
+     * @return true if the email is sent and false if there is error in sending.
+     */
+    public static boolean sendEmail(String subject, String content, String recipientAddress){
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(SENDER_ADDR, PASSWORD);
+                    }
+                });
+
+        try{
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(SENDER_ADDR));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(recipientAddress));
+            message.setSubject(subject);
+            message.setText(content);
+
+            Transport.send(message);
+
+        }catch (MessagingException e){
+            System.err.println("EMAIL SEND FAILURE DUE TO: ");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+//    public static void main(String args[]){
+//        EmailUtil.sendEmail("Registration Confirmation","yes its ok","liquansyd@gmail.com");
+//    }
+}
