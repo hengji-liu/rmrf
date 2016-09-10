30c65e4ec8e2a320d3b2fee0f2a8d9f7f7bfbf59
ge90toga <liquan1992@outlook.com>
2016-09-10 14:59:42 +1000

    initial admin login

--- src/service/AdminManager.java
+++ src/service/AdminManager.java
@@ -1,8 +1,15 @@
 package service;
 
+import daoImpl.AdminDaoImpl;
+import daoIterface.AdminDao;
+
 /**
  * Created by Linus on 10/09/2016.
  */
 public class AdminManager {
 
+    public boolean adminLogin(String userName,String password){
+        AdminDao adminDao = new AdminDaoImpl();
+        return adminDao.adminLogin(userName,password);
+    }
 }
---
+++ web/admin.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Linus
+  Date: 10/09/2016
+  Time: 2:54 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Managing BBLIB</title>
+</head>
+<body>
+<h1> Hello Admin</h1>
+
+</body>
+</html>
